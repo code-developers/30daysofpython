217826ad35b6dcca58609a5eed0b480756d69384
krishpranav <krisna.pranav@gmail.com>
2021-03-22 15:44:59 +0530

    done changes

--- 1dayofpython/hello.py
+++ 1dayofpython/hello.py
@@ -3,3 +3,10 @@
 #print function
 print("hello world") #you can put what ever values you like inside the colen
 #this print function is used to print out values and response from api
+
+
+#---------------SIMPLE IF STATEMENT------------------------------------
+if 5 > 2:
+    print("Five is greater than two")
+
+#the expansion is if 5 is greater than two means it will print("five is greater than two")
