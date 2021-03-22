1a449b8e4271b9f5e1adb5e3ca3457074efef430
krishpranav <krisna.pranav@gmail.com>
2021-03-22 15:47:57 +0530

    done changes

--- 1dayofpython/hello.py
+++ 1dayofpython/hello.py
@@ -1,7 +1,8 @@
 #first lesson of python
 
 #print function
-print("hello world") #you can put what ever values you like inside the colen
+print("hello world")
+ #you can put what ever values you like inside the colen
 #this print function is used to print out values and response from api
 
 
