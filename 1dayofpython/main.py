2a47d09996da47c1acf8a8bc67afa1e18d19f04c
krishpranav <krisna.pranav@gmail.com>
2021-06-23 18:59:03 +0530

    [+] main [+]

---
+++ 1dayofpython/main.py
@@ -0,0 +1,7 @@
+#first lesson of python
+
+#print function
+print("hello world")
+ #you can put what ever values you like inside the colen
+#this print function is used to print out values and response from api
+
