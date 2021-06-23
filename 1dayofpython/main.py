5e22f461c2c1e6675236a67f6a8dfd8a4706e746
krishpranav <krisna.pranav@gmail.com>
2021-06-23 19:00:09 +0530

    [+] update main [+]

--- 1dayofpython/main.py
+++ 1dayofpython/main.py
@@ -5,3 +5,7 @@ print("hello world")
  #you can put what ever values you like inside the colen
 #this print function is used to print out values and response from api
 
+
+# variables
+names = ['alexis', 'john']
+print(names)
